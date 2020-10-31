fa5b8266145bbb27cf7446e5c2de56fa362a6019
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2020-11-01 02:50:56 +0300

    svelte-counter project added

---
+++ src/Counter.svelte
@@ -0,0 +1,17 @@
+<script>
+  let counter = 0;  // counter variable
+  const handleClick = () => { //Function to add 1 to our variable
+    counter += 1;
+  };
+</script>
+<style>
+  button{
+    border:0px;
+    background: #3380ef;
+    color:white;
+  }
+</style>
+<div class=”counterComponent”>
+  <button on:click={handleClick}>plus 1</button>
+  <section>My couter is: {counter}</section>
+</div>
